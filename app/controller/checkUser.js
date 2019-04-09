c1453dff8de6fd8cb5eb17942d706729178b5ca2
chiove <240718742@qq.com>
2019-04-09 14:09:59 +0800

    配置eslint

--- app/controller/checkUser.js
+++ app/controller/checkUser.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict';
 
 const Controller = require('egg').Controller;
-class checkUserController extends Controller{
-    async index(){
-        const ctx = this.ctx;
-        const result = await ctx.service.checkUser.getUserInfo(ctx.session);
-        ctx.body = {
-          message:'login success',
-          status:true,
-          data:result
-        };
-    }
+class checkUserController extends Controller {
+  async index() {
+    const ctx = this.ctx;
+    const result = await ctx.service.checkUser.getUserInfo(ctx.session);
+    ctx.body = {
+      message: 'login success',
+      status: true,
+      data: result,
+    };
+  }
 }
 
-module.exports =  checkUserController;
+module.exports = checkUserController;
