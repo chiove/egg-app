5e3d7a65ae2cd521c9c0661318db96a9f48132cf
chiove <240718742@qq.com>
2019-04-09 14:07:13 +0800

    配置eslint

--- app/service/apiManagement.js
+++ app/service/apiManagement.js
@@ -1,36 +1,33 @@
-"use strict";
+'use strict';
 
 const Service = require('egg').Service;
-class apiManagementService extends Service{
-    constructor(ctx) {
-        super(ctx);
-    }
-    async index(params){
-        let res = {
-            code:0,
-            message:'success'
-        };
-        if(Object.keys(params).length===0){
-            res.data = await this.app.mysql.select('api_management',{limit: 10, offset: 0,});
-        }else{
-            res.data = await this.app.mysql.select('api_management',{
-                where:{
-                    ...params
-                },
-                limit: 10,
-                offset: 0,
-            });
-        }
-        return res;
+class apiManagementService extends Service {
+  async index(params) {
+    const res = {
+      code: 0,
+      message: 'success',
     };
-    async create(params){
-        let res = {
-            code:0,
-            message:'success'
-        };
-        await this.app.mysql.insert('api_management',params);
-        return res;
+    if (Object.keys(params).length === 0) {
+      res.data = await this.app.mysql.select('api_management', { limit: 10, offset: 0 });
+    } else {
+      res.data = await this.app.mysql.select('api_management', {
+        where: {
+          ...params,
+        },
+        limit: 10,
+        offset: 0,
+      });
     }
+    return res;
+  }
+  async create(params) {
+    const res = {
+      code: 0,
+      message: 'success',
+    };
+    await this.app.mysql.insert('api_management', params);
+    return res;
+  }
 }
 
 module.exports = apiManagementService;
--- app/service/checkUser.js
+++ app/service/checkUser.js
@@ -1,14 +1,11 @@
-"use strict";
+'use strict';
 
 const Service = require('egg').Service;
-class checkUserService extends Service{
-    constructor(ctx) {
-        super(ctx);
-    }
-    async getUserInfo(params){
-        const userInfo = await this.app.mysql.get('user',{account:params.account});
-        return userInfo;
-    }
+class checkUserService extends Service {
+  async getUserInfo(params) {
+    const userInfo = await this.app.mysql.get('user', { account: params.account });
+    return userInfo;
+  }
 }
 
 module.exports = checkUserService;
